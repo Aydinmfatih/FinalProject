2d636cd3a61a93fac53e365ec120fa38e9e55245
aydinmfatih <aydinmfatih@gmail.com>
2023-06-07 04:12:54 +0300

    Ioc Configured on business Layer

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,15 +1,27 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolver.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 
+//Ioc service provider
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutoFacBusinessModule());
+    });
+
+
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService,ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+//builder.Services.AddSingleton<IProductService,ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
