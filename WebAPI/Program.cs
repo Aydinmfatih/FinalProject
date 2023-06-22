d2235735d678fef87ec0ac7032a8db35bf9d8c53
aydinmfatih <aydinmfatih@gmail.com>
2023-06-22 17:52:45 +0300

    .

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -34,6 +35,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            
 
 app.UseHttpsRedirection();
 
